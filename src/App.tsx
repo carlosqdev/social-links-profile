ccc1fac407cf95e976315069c059b4b5b25f565b
carlosqdev <carlos.quintero@something.com>
2024-02-06 19:55:03 -0600

    add structure html

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main>
+      <section>
+        <img src="https://avatars.githubusercontent.com/u/65479034?v=4" alt="Carlos Quintero" />
+
+        <hgroup>
+          <h1>Carlos Quintero</h1>
+          <p>Managua, Nicaragua</p>
+        </hgroup>
+
+        <p>Full Stack developer and Master Pokemon</p>
+
+        <ul>
+          <li>
+            <a href="">GitHub</a>
+          </li>
+
+          <li>
+            <a href="">Frontend Mentor</a>
+          </li>
+
+          <li>
+            <a href="">Linkedin</a>
+          </li>
+
+          <li>
+            <a href="">Twitter</a>
+          </li>
+
+          <li>
+            <a href="">Instagram</a>
+          </li>
+
+        </ul>
+      </section>
+    </main>
   )
 }
