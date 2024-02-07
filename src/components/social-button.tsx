01308fa2f01b2aee34934c857faa30f44a12cbbf
carlosqdev <carlos.quintero@something.com>
2024-02-06 20:49:27 -0600

    add hover social button

--- src/components/social-button.tsx
+++ src/components/social-button.tsx
@@ -2,7 +2,7 @@ import { ISocial } from "../types";
 
 export default function SocialButton({ name, href }: ISocial) {
   return (
-    <li className="bg-gray-600 text-[14px] font-medium rounded-md p-2 text-center">
+    <li className="bg-gray-600 text-[14px] font-medium rounded-md p-2 text-center hover:bg-gray-700">
       <a href={href}>{name}</a>
     </li>
   )
